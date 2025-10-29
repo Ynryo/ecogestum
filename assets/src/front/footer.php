ad164c3728483b332573557cc571672f53250d31
Ynryo <ewenatideklein@gmail.com>
2025-10-30 00:08:36 +0100

    commit

---
+++ assets/src/front/footer.php
@@ -0,0 +1,34 @@
+<footer>
+    <div>
+        <img src="/assets/img/lmu-logo-white.png" alt="Logo de Le Mans Université">
+        <p>Avenue Olivier Messiaen 72085 Le Mans CEDEX 9</p>
+        <a href="tel:+33243833000" class="button">+33 2 43 83 30 00</a>
+        <a href="https://www.univ-lemans.fr/fr/contact-1.html" class="button">Nous contacter</a>
+        <a href="https://www.univ-lemans.fr/fr/pied-de-page/plans-des-campus.html" class="button">Plan des campus</a>
+    </div>
+    <div>
+        <h4>Accès rapides</h4>
+        <a href="" class="link">Tous nos sites web</a>
+        <a href="" class="link">Espace presse</a>
+        <a href="" class="link">Offres d'emploi</a>
+        <a href="" class="link">Marchés publics</a>
+        <a href="" class="link">Charte graphique</a>
+    </div>
+    <div>
+        <h4>Restons connectés</h4>
+        <div class="socials">
+            <a href="https://www.instagram.com/lemansuniversite/?hl=fr" class="link">
+                <img src="/assets/img/socials/instagram.png" alt="Logo d'Instagram">
+            </a>
+            <a href="https://www.linkedin.com/school/universit%C3%A9-du-maine-le-mans-laval/mycompany/" class="link">
+                <img src="/assets/img/socials/linkedln.png" alt="Logo de LinkedIn">
+            </a>
+            <a href="https://www.youtube.com/channel/UCGcQ5RW44I0xEIg4pNS69pg" class="link">
+                <img src="/assets/img/socials/youtube.png" alt="Logo de YouTube">
+            </a>
+            <a href="https://www.facebook.com/LeMansUniversite" class="link">
+                <img src="/assets/img/socials/facebook.png" alt="Logo de Facebook">
+            </a>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <div class="top-container">
+    <section class="top-container">
         <?php include(dirname(__FILE__, 1) . '/assets/src/front/header.php') ?>
         <div class="top-content">
             <div>
@@ -21,7 +21,38 @@
                 <a href="/login/" class="button">Rejoindre</a>
             </div>
         </div>
-    </div>
+    </section>
+    <section class="keynums">
+        <div class="blue-bg">
+            <h1>18.054</h1>
+            <p>objets déjà recyclés</p>
+        </div>
+        <div class="red-bg">
+            <h1>1.569</h1>
+            <p>objets disponibles actuellement</p>
+        </div>
+        <div class="green-bg">
+            <h1>19.058</h1>
+            <p>tonnes équivalent CO2 économisés chaque année</p>
+        </div>
+    </section>
+    <section class="they-speak">
+        <h2>Ils parlent de nous</h2>
+        <div class="cards-container">
+            <!-- A faire en php -->
+            <div class="card">
+                <img src="/assets/img/portraits/theo.png" alt="Portrait de Théo">
+                <h3>Théo M., étudiant à l’ENSIM</h3>
+                <p>Le concept de EcoGestUM est incroyable. Il m'a permis de donner mon ordinateur à une autre personne au lieu de le jeter.</p>
+            </div>
+        </div>
+    </section>
+    <section class="press-releases">
+        <h2>Communiqués de la direction de l’Université</h2>
+        <!-- A faire en php -->
+         <a href="/press-releases" class="button">Voir plus de communiqués</a>
+    </section>
+    <?php include(dirname(__FILE__, 1) . '/assets/src/front/footer.php') ?>
 </body>
 
 </html>
